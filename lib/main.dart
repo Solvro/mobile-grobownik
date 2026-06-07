cefaea1bf62cb77059c1bd43632bbef634ac74c7
Szymon Kowaliński <szymon@kowalinski.dev>
2026-06-07 23:26:19 +0200

    chore: install solvro config and bump flutter (#1)
    * chore: install solvro config and bump flutter
    
    * chore: fix lint rules
    
    * chore: android migration
    
    * chore: new solvro config
    
    * chore: drop legacy loc commands
    
    * chore: new solvro config
    
    * chore: new solvro config
    
    * chore: add auto route generator

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,22 @@
-import 'package:flutter/material.dart';
+import "dart:async";
+
+import "package:flutter/material.dart";
+import "package:flutter/services.dart";
+import "package:hooks_riverpod/hooks_riverpod.dart";
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: "Flutter Demo",
+      theme: ThemeData(colorScheme: const ColorScheme.light()),
+      home: const MyHomePage(title: "Flutter Demo Home Page"),
     );
   }
 }
@@ -54,7 +40,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  var _counter = 0;
 
   void _incrementCounter() {
     setState(() {
@@ -104,18 +90,18 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: .center,
           children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            const Text("You have pushed the button this many times:"),
+            Text("$_counter", style: Theme.of(context).textTheme.headlineMedium),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: () {
+          unawaited(HapticFeedback.selectionClick());
+          _incrementCounter();
+        },
+        tooltip: "Increment",
+        child: const Icon(Icons.add, semanticLabel: "Increment counter"),
       ),
     );
   }
