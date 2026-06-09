66ff673c0c9d228318f4d75f31b9c4bbb3293cef
M1K45 <272570@student.pwr.edu.pl>
2026-06-09 22:41:46 +0200

    refactor: add components to separate fiels

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -2,9 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
-class Palette {
-  // Pamiętaj, że we Flutterze kody HEX zapisujemy z prefiksem 0xFF (gdzie FF to 100% krycia)
+abstract class ColorsConsts {
   static const Color inkBlack = Color.fromARGB(255, 17, 21, 28);
-
   static const Color charcoalBlue = Color.fromARGB(255, 54, 65, 86);
 }
