48c66af5202e298013717a80372a8ef5d4984ccf
tomasz-trela <trela.tomasz@protonmail.com>
2026-06-14 16:27:09 +0200

    chore: unify string quotes in example test

--- test/example_test.dart
+++ test/example_test.dart
@@ -1,7 +1,7 @@
-import 'package:flutter_test/flutter_test.dart';
+import "package:flutter_test/flutter_test.dart";
 
 void main() {
-  test('one plus one equals two', () {
+  test("one plus one equals two", () {
     expect(1 + 1, equals(2));
   });
 }
