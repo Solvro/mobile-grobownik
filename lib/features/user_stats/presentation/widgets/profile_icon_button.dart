ed67f15d07e06ddf33caf3923017a9f470295e54
Dawid Zając <ziutekxd56@gmail.com>
2026-08-10 22:55:04 +0200

    feat: add profile icon navigation

--- lib/features/user_stats/presentation/widgets/profile_icon_button.dart
+++ lib/features/user_stats/presentation/widgets/profile_icon_button.dart
@@ -1,8 +1,16 @@
 import "package:flutter/material.dart";
 import "package:flutter/services.dart";
 
+<<<<<<< HEAD:lib/features/user_stats/presentation/widgets/profile_icon_button.dart
 import "../../../../app/l10n/app_localizations.dart";
 import "../../../../app/theme/app_theme.dart";
+=======
+import "../../l10n/app_localizations.dart";
+import "../../services/auth_service.dart";
+import "../../theme/app_theme.dart";
+import "../login_view.dart";
+import "../user_stats_view.dart";
+>>>>>>> 0e50747 (feat: add profile icon navigation):lib/widgets/detail_views/profile_icon_widget.dart
 
 class ProfileIconWidget extends StatelessWidget {
   @override
@@ -14,6 +22,15 @@ class ProfileIconWidget extends StatelessWidget {
         tooltip: AppLocalizations.of(context)!.profile,
         onPressed: () async {
           await HapticFeedback.selectionClick();
+
+          final loggedIn = await AuthService.isLoggedIn();
+          if (!context.mounted) return;
+
+          if (loggedIn) {
+            await Navigator.push(context, MaterialPageRoute<void>(builder: (context) => const UserStatsPage()));
+          } else {
+            await Navigator.push(context, MaterialPageRoute<void>(builder: (context) => const LoginScreen()));
+          }
         },
         icon: Icon(
           Icons.person_outline,
